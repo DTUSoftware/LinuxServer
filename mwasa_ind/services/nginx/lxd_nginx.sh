4ef4efbb35ac0f39fd0af2b016f7b13838205cc9
HeroGamers <hero@herogamers.dev>
2023-05-12 17:58:07 +0200

    I really need to turn on autosave on vscode

--- mwasa_ind/services/nginx/lxd_nginx.sh
+++ mwasa_ind/services/nginx/lxd_nginx.sh
@@ -6,5 +6,5 @@ lxc exec nginx -- apt-get update
 lxc exec nginx -- apt-get install nginx -y
 lxc file push -r ./www/nuclear-codes nginx/var/www
 lxc file push -r ./nginx/sites-enabled nginx/etc/nginx
-lxc exec nginx -- mv /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default 
+lxc exec nginx -- rm /etc/nginx/sites-enabled/default
 lxc exec nginx -- service nginx restart
