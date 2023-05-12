bb230042eb427ce20d25dae811bbbfb78c3095af
HeroGamers <hero@herogamers.dev>
2023-05-12 17:22:56 +0200

    mmmmh, docker-compose is better, fuck lxd

--- mwasa_ind/services/nginx/lxd_nginx.sh
+++ mwasa_ind/services/nginx/lxd_nginx.sh
@@ -1,6 +1,7 @@
 lxc launch ubuntu:18.04 nginx
-lxc start nginx
 lxc network attach nginx macvlan eth0
+lxc stop nginx
+lxc start nginx
 lxc exec nginx -- apt-get update
 lxc exec nginx -- apt-get install nginx -y
 lxc file push -r ./www/nuclear-codes nginx/var/www/nuclear-codes
